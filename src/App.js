f1815c86c72ab2db78ac308ab6872e9e81f264cb
LeoWu <wzc1984@gmail.com>
2017-08-30 12:02:19 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './Search';
+import ResultList from './ResultList';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      orderList: []
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Search orderList={this.state.orderList} />
+        <ResultList />
       </div>
     );
   }
